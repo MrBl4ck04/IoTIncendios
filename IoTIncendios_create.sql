54dcc105698e4c149d5e0541f58fcc3950c00cdc
DAGod <90727076+inaDAGod@users.noreply.github.com>
2024-11-11 22:27:18 -0400

    Update IoTIncendios_create.sql

--- IoTIncendios_create.sql
+++ IoTIncendios_create.sql
@@ -7,6 +7,7 @@ CREATE TABLE co2 (
     fecha date  NOT NULL,
     hora time  NOT NULL,
     valor double(30,3)  NOT NULL,
+    microcontroladores_id int  NOT NULL,
     CONSTRAINT co2_pk PRIMARY KEY (id)
 );
 
@@ -16,6 +17,7 @@ CREATE TABLE humedad (
     fecha date  NOT NULL,
     hora time  NOT NULL,
     valor double(30,3)  NOT NULL,
+    microcontroladores_id int  NOT NULL,
     CONSTRAINT humedad_pk PRIMARY KEY (id)
 );
 
@@ -25,7 +27,16 @@ CREATE TABLE humo (
     fecha date  NOT NULL,
     hora time  NOT NULL,
     valor double(30,3)  NOT NULL,
+    microcontroladores_id int  NOT NULL,
     CONSTRAINT humo_pk PRIMARY KEY (id)
+);
+
+-- Table: microcontroladores
+CREATE TABLE microcontroladores (
+    id int  NOT NULL AUTO_INCREMENT,
+    nombre varchar(50)  NOT NULL,
+    ubicaciones_id int  NOT NULL,
+    CONSTRAINT microcontroladores_pk PRIMARY KEY (id)
 );
 
 -- Table: temperatura
@@ -34,7 +45,15 @@ CREATE TABLE temperatura (
     fecha date  NOT NULL,
     hora time  NOT NULL,
     valor double(30,3)  NOT NULL,
+    microcontroladores_id int  NOT NULL,
     CONSTRAINT temperatura_pk PRIMARY KEY (id)
+);
+
+-- Table: ubicaciones
+CREATE TABLE ubicaciones (
+    id int  NOT NULL AUTO_INCREMENT,
+    descripcion varchar(200)  NOT NULL,
+    CONSTRAINT ubicaciones_pk PRIMARY KEY (id)
 );
 
 -- Table: usuarios
@@ -44,6 +63,27 @@ CREATE TABLE usuarios (
     password varchar(30)  NOT NULL,
     CONSTRAINT usuarios_pk PRIMARY KEY (id)
 );
+
+-- foreign keys
+-- Reference: co2_microcontroladores (table: co2)
+ALTER TABLE co2 ADD CONSTRAINT co2_microcontroladores FOREIGN KEY co2_microcontroladores (microcontroladores_id)
+    REFERENCES microcontroladores (id);
+
+-- Reference: humedad_microcontroladores (table: humedad)
+ALTER TABLE humedad ADD CONSTRAINT humedad_microcontroladores FOREIGN KEY humedad_microcontroladores (microcontroladores_id)
+    REFERENCES microcontroladores (id);
+
+-- Reference: humo_microcontroladores (table: humo)
+ALTER TABLE humo ADD CONSTRAINT humo_microcontroladores FOREIGN KEY humo_microcontroladores (microcontroladores_id)
+    REFERENCES microcontroladores (id);
+
+-- Reference: microcontroladores_ubicaciones (table: microcontroladores)
+ALTER TABLE microcontroladores ADD CONSTRAINT microcontroladores_ubicaciones FOREIGN KEY microcontroladores_ubicaciones (ubicaciones_id)
+    REFERENCES ubicaciones (id);
+
+-- Reference: temperatura_microcontroladores (table: temperatura)
+ALTER TABLE temperatura ADD CONSTRAINT temperatura_microcontroladores FOREIGN KEY temperatura_microcontroladores (microcontroladores_id)
+    REFERENCES microcontroladores (id);
 
 -- End of file.
 
