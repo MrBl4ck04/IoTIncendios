99042f01d2b932352782c7531d3df7cb1f15ac61
inaDAGod <danialee14@gmail.com>
2024-12-03 20:32:00 -0400

    add dashboard to navbars

--- microWeb/microABM.php
+++ microWeb/microABM.php
@@ -92,6 +92,15 @@ $result = $conn->query($sql);
                             <a class="dropdown-item" href="http://localhost/IoTIncendios/datosWeb/ver_datos.php?sensor=temperatura">Temperatura</a>
                         </div>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            DashBoards
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="http://localhost/IoTIncendios/dashboard/DashFechaValor.php">Fechas</a>
+                            <a class="dropdown-item" href="http://localhost/IoTIncendios/dashboard/DashUbicacionValor.php">Ubicaciones</a>    
+                        </div>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="http://localhost/IoTIncendios/contacto/indexContactoAD.html">Contacto</a>
                     </li>
